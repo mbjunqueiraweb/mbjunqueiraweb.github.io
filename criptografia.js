5a8b77a6a1ee8a1b63904e135764ed484d79f827
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-12 17:06:19 -0300

    tentativa

---
+++ criptografia.js
@@ -0,0 +1,8 @@
+key = [AB5-H7G-F9DL-ET3N]
+cript (key) = {
+  for (i = 0, i < key.length(), i += 1) key[i].set(i+1)  
+}
+
+decript (key) = {
+  for (i = 0, i < key.length(), i += 1) key[i].set(i-1)
+}
